007055f4e76054bf148deb4674226908f7a6f512
nazarov <collermix@gmail.com>
2023-08-29 15:27:10 +0600

    Add comments

--- cmd/main.go
+++ cmd/main.go
@@ -9,20 +9,24 @@ import (
 func main() {
 	initDB()
 	router := httprouter.New()
+
+	// users routes
 	router.GET("/users", getUsers)
 	router.POST("/users", createUser)
 	router.GET("/users/:id", getUser)
 	router.PUT("/users/:id", updateUser)
 	router.DELETE("/users/:id", deleteUser)
 
+	// slugs routes
 	router.GET("/segments", getSegments)
 	router.POST("/segments", createSegment)
 	router.PUT("/segments/:slug", updateSegment)
 	router.DELETE("/segments/:slug", deleteSegment)
 
+	// add and delete user slugs route
 	router.POST("/user_segments", addSegmentsToUser)
 
-	log.Println("Сервер запущен на порту :8000")
+	log.Println("Server listen and serve on port :8000")
 	err := http.ListenAndServe(":8000", router)
 	if err != nil {
 		log.Fatal(err)
